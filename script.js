2ce3561cb345c4cdb87e225de87a573d935b971a
muneebsaleem425 <97957398+muneebsaleem425@users.noreply.github.com>
2023-12-29 12:20:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ 
+  	if(a == "Muneeb")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
